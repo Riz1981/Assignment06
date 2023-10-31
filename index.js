60a379c13b0de4409576d385d2fcafc8e597aecf
Rizwan Ahmad <113388795+Riz1981@users.noreply.github.com>
2023-10-31 11:44:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+//print name of a person with whitespace \t \n usning & trim striped:
+var nameWithWhitespace = "\t  \nRizwan ahmed\t  \n";
+console.log("name with white space" + nameWithWhitespace);
+var strippedName = nameWithWhitespace.trim();
+console.log("stripped name :" + strippedName);
---
+++ index.ts
@@ -0,0 +1,6 @@
+//print name of a person with whitespace \t \n usning & trim striped:
+let nameWithWhitespace:string=`\t  \nRizwan ahmed\t  \n`;
+console.log("name with white space" + nameWithWhitespace);
+let strippedName:string = nameWithWhitespace.trim();
+console.log("stripped name :"+strippedName);
+
